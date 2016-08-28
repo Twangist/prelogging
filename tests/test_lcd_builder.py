67ab673a6cc0992206f4b5b3eecb9967955c89a4
Brian O'Neill <twangist@yahoo.com>
2016-08-28 14:10:50 -0400

    0.2.7b24 -- renamed tests/_testlogs/configurator directory to ....../builder
    TODO: docs, one more example, etc.
    TODO: PERHAPS rename
        lcd      =>   logging_config
        LCD     =>   LCDict_
        LCDEx  =>   LCDict

--- tests/test_lcd_builder.py
+++ tests/test_lcd_builder.py
@@ -22,8 +22,7 @@ import test_lcd_builder_SubB
 
 def test_builder():
     """
-    >>> LOG_PATH = '_testlogs/configurator'     # TODO rename dir & here '.../builder'
-
+    >>> LOG_PATH = '_testlogs/builder'
     >>> lcdx = LCDBuilder.build_lcd(
     ...     root_level='WARNING',
     ...     log_path=LOG_PATH,
@@ -91,70 +90,5 @@ def load_tests(loader, tests, ignore):
     return tests
 
 if __name__ == "__main__":
-
-    # LOG_PATH = '_testlogs/configurator'
-    #
-    # from test_configurators_top import LCDBuilder
-    #
-    # LCDBuilder.build_lcd(
-    #     root_level='WARNING',
-    #     log_path=LOG_PATH,
-    #     locking=False,
-    #     attach_handlers_to_root=False)
-    #
-    # # Now log some messages
-    #
-    # root = logging.getLogger()
-    # loggerA = logging.getLogger('subA')
-    # loggerB = logging.getLogger('subB')
-    #
-    # root.debug(   "0.")
-    # root.info(    "1.")
-    # root.warning( "2.")
-    # # root                : WARNING : 2.
-    # root.error(   "3.")
-    # # root                : ERROR   : 3.
-    # root.critical("4.")
-    # # root                : CRITICAL: 4.
-    #
-    # loggerA.debug(   "10.")
-    # loggerA.info(    "11.")
-    # loggerA.warning( "12.")
-    # loggerA.error(   "13.")
-    # # subA                : ERROR   : 13.
-    # loggerA.critical("14.")
-    # # subA                : CRITICAL: 14.
-    #
-    # loggerB.debug(   "20.")
-    # loggerB.info(    "21.")
-    # loggerB.warning( "22.")
-    # loggerB.error(   "23.")
-    # loggerB.critical("24.")
-    #
-    # # Check logfile contents
-    #
-    # import os
-    # with open(os.path.join(LOG_PATH, 'subA.log')) as f:
-    #     text_A = f.read()
-    # print(
-    #     text_A == ("subA                : ERROR   : 13.\n"
-    #                "subA                : CRITICAL: 14.\n")
-    # )
-    # # True
-    #
-    # with open(os.path.join(LOG_PATH, 'subB.log')) as f:
-    #     text_B = f.read()
-    # print(
-    #     text_B == ("subB                : DEBUG   : 20.\n"
-    #                "subB                : INFO    : 21.\n"
-    #                "subB                : WARNING : 22.\n"
-    #                "subB                : ERROR   : 23.\n"
-    #                "subB                : CRITICAL: 24.\n")
-    # )
-    # # True
-    #
-    # exit(1)
-
-    ####################################
 
     doctest.testmod()   # (verbose=True)
